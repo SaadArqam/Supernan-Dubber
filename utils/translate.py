73a7d307335377048bcb87f6987093766d6a3591
Saad Arqam <saadarqamrnc@gmail.com>
2026-02-24 11:32:23 +0530

    fix: noisy translation

--- utils/translate.py
+++ utils/translate.py
@@ -6,31 +6,32 @@ model_name = "facebook/nllb-200-distilled-600M"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-def translate_to_hindi(text, detected_lang_code):
-    print(f"Translating from {detected_lang_code} → Hindi...")
+lang_map = {
+    "en": "eng_Latn",
+    "kn": "kan_Knda",
+    "hi": "hin_Deva",
+    "ta": "tam_Taml",
+    "te": "tel_Telu",
+    "ml": "mal_Mlym",
+    "fr": "fra_Latn",
+    "de": "deu_Latn",
+    "es": "spa_Latn"
+}
 
-    # Map whisper language code to NLLB language code
-    lang_map = {
-        "en": "eng_Latn",
-        "kn": "kan_Knda",
-        "hi": "hin_Deva",
-        "ta": "tam_Taml",
-        "te": "tel_Telu",
-        "ml": "mal_Mlym",
-        "fr": "fra_Latn",
-        "de": "deu_Latn",
-        "es": "spa_Latn"
-    }
+def translate_to_hindi(text, detected_lang):
+    print(f"Translating from {detected_lang} → Hindi...")
 
-    src_lang = lang_map.get(detected_lang_code, "eng_Latn")
+    src_lang = lang_map.get(detected_lang, "eng_Latn")
 
     tokenizer.src_lang = src_lang
 
     encoded = tokenizer(text, return_tensors="pt")
 
+    hindi_token_id = tokenizer.convert_tokens_to_ids("hin_Deva")
+
     generated_tokens = model.generate(
         **encoded,
-        forced_bos_token_id=tokenizer.lang_code_to_id["hin_Deva"],
+        forced_bos_token_id=hindi_token_id,
         max_length=512
     )
 
