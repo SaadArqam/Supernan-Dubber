6a311d6da608da6010f74cb7a04b6119100de407
Saad Arqam <saadarqamrnc@gmail.com>
2026-02-24 11:27:52 +0530

    fix: language repetation

--- utils/transcribe.py
+++ utils/transcribe.py
@@ -1,20 +1,16 @@
 from faster_whisper import WhisperModel
 
+model = WhisperModel("medium", compute_type="float16")
 
 def transcribe_audio(audio_path):
-    model = WhisperModel("medium", compute_type="float32")
+    segments, info = model.transcribe(audio_path)
 
-    segments, info = model.transcribe(
-    audio_path,
-    task="transcribe",
-    vad_filter=False
-)
-
-    print("Detected language:", info.language)
-
-    full_text = ""
+    text = ""
     for segment in segments:
-        print(f"[{segment.start:.2f}s → {segment.end:.2f}s] {segment.text}")
-        full_text += segment.text + " "
+        text += segment.text + " "
 
-    return full_text.strip()
+    detected_language = info.language
+
+    print(f"Detected language: {detected_language}")
+
+    return text.strip(), detected_language
